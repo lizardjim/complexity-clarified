e6870a2ec8b6c7a8e1ae321e7e51bd4e163e6967
James Barnett <james-barnett@outlook.com>
2026-08-06 12:55:36 +0100

    Further updates to contact.js

--- functions/api/contact.js
+++ functions/api/contact.js
@@ -4,19 +4,42 @@ export async function onRequestPost(context) {
 
         const body = await context.request.json();
 
+        const { name, email, message } = body;
+
+        if (!name || !email || !message) {
+
+            return Response.json(
+                {
+                    success: false,
+                    error: "All fields are required."
+                },
+                {
+                    status: 400
+                }
+            );
+
+        }
+
         return Response.json({
             success: true,
-            received: body
+            received: {
+                name,
+                email,
+                message
+            }
         });
 
     } catch {
 
-        return Response.json({
-            success: false,
-            error: "Invalid JSON"
-        }, {
-            status: 400
-        });
+        return Response.json(
+            {
+                success: false,
+                error: "Invalid request."
+            },
+            {
+                status: 400
+            }
+        );
 
     }
 
